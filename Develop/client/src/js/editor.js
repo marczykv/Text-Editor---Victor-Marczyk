dae27ad137c8b117f073162282a0bff0ac7cea65
Victor Marczyk <marczykv@gmail.com>
2023-11-01 21:30:04 -0400

    implemented putDb and getDb

--- Develop/client/src/js/editor.js
+++ Develop/client/src/js/editor.js
@@ -1,13 +1,14 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
 import { openDB } from 'idb';
-import { getDb, putDb } from './database';
-import { header } from './header';
+
+// Imports database functions and header
+import { getDb, putDb } from './database'; // Imports functions
+import { header } from './header'; // Imports content
 
 export default class {
   constructor() {
     const localData = localStorage.getItem('content');
 
-    // check if CodeMirror is loaded
+    // Check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
@@ -23,8 +24,48 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    // Initialize the IndexedDB database
+    const initdb = async () => {
+      openDB('jate', 1, {
+        upgrade(db) {
+          if (db.objectStoreNames.contains('jate')) {
+            console.log('jate database already exists');
+            return;
+          }
+          db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+          console.log('jate database created');
+        },
+      });
+    };
+
+    // Implement a method to save content to IndexedDB
+    const putDb = async (content) => {
+      const db = await openDB('jate', 1);
+      const tx = db.transaction('jate', 'readwrite');
+      const store = tx.objectStore('jate');
+      const timestamp = Date.now();
+
+      // Add the content to the object store
+      await store.put({ id: timestamp, content });
+
+      await tx.done;
+    };
+
+    // Implement a method to get content from IndexedDB
+    const getDb = async () => {
+      const db = await openDB('jate', 1);
+      const tx = db.transaction('jate', 'readonly');
+      const store = tx.objectStore('jate');
+
+      // Retrieve the content from the object store (latest entry)
+      const latestEntry = await store.get('latest');
+
+      return latestEntry ? latestEntry.content : null;
+    };
+
+    // Fall back to localStorage if nothing is stored in IndexedDB, and if neither is available, set the value to header.
+    initdb(); // Initialize the IndexedDB database
+
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
       this.editor.setValue(data || localData || header);
@@ -34,7 +75,7 @@ export default class {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+    // Save the content of the editor when the editor itself loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
       putDb(localStorage.getItem('content'));
